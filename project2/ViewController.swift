ac0654025883f1a21df0e104e75668e9ea66f8f0
Parth Dalwadi <pbdalwadi@gmail.com>
2020-09-01 23:23:54 -0400

    Initial Commit

---
+++ project2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project2
+//
+//  Created by Parth Dalwadi on 2020-09-01.
+//  Copyright © 2020 Parth Dalwadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
